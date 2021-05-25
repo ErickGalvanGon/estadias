70b409a36519c2a0c095423050808b584b8b8d01
Rene Vazquez <renevr4@gmail.com>
2021-05-24 20:30:11 -0600

    Le agregue mi nombre

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    Erick Galvan hizo esto
+    <div>Erick Galvan hizo esto</div>
+    <div>Rene Vazquez hizo esto</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
